5eea45ff907b13bfcf4d0191b7c6f7fafe521d8f
sunilvaranasi27 <sunil.varanasi@gmail.com>
2020-08-09 11:57:08 -0500

    1 file added

---
+++ Register.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+       <form method="" action="">
+        <table border="1" align="center" height="100" width="500" bgcolor="#CCD4DD">
+            <tr>
+                <th> Welcome to Registration Page! Please click on the below links
+                    <ul>
+
+                        <li>
+                            <div align="justify">
+                                <a href="Index.js">Register</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div align="justify">
+                                <a href="Display Records.html">Display record </a>
+                            </div>
+                        </li>
+                    </ul>
+                </th>
+            </tr>
+
+        </table>
+    </form>
+            
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'));
